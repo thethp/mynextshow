1d540197fd0ceba11e0189e2d1b2516d7782a064
safe user <todderick@gmail.com>
2014-10-08 19:05:34 -0400

    Use one page, make it so that if anyone changes css it will obsessively fit my needs

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp = require('gulp'),
+    comb = require('gulp-csscomb'),
     less = require('gulp-less'),
     hint = require('gulp-jshint'),
     minifyCSS = require('gulp-minify-css'),
     path = require('path');
 
-gulp.task('default', ['less', 'lint']);
+gulp.task('default', ['comb','less', 'lint']);
+
+gulp.task('comb', function() {
+  return gulp.src('./styles/*.less')
+    .pipe(comb())
+    .pipe(gulp.dest('./styles/'));
+});
 
 gulp.task('less', function() {
-  gulp.src(['./styles/main.less'])
+  return gulp.src('./styles/main.less')
     .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('lint', function() {
-  gulp.src(['./app.js','./routes.js','./lib/*.js','./lib/models/*.js'])
+  return gulp.src(['./app.js','./routes.js','./lib/*.js','./lib/models/*.js'])
     .pipe(hint())
     .pipe(hint.reporter('default'));
 });
